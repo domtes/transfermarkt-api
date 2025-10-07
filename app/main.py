7224ac3a4744154f31890cd18a09a0fa70927f4a
Domenico Testa <domenico.testa@gmail.com>
2025-10-08 01:10:28 +0200

    fix: run in production mode

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+import os
 import uvicorn
+
 from fastapi import FastAPI
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
@@ -8,6 +10,9 @@ from starlette.responses import RedirectResponse
 
 from app.api.api import api_router
 from app.settings import settings
+
+
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
 
 limiter = Limiter(
     key_func=get_remote_address,
@@ -27,4 +32,4 @@ def docs_redirect():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=DEBUG)
